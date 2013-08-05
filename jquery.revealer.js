fbe659a0e0cd14dd49b582e946ff8caad23d58a1
Christopher Hayes <chris@cthayes.com>
2013-08-05 10:09:08 -0400

    Update jquery.revealer.js

--- jquery.revealer.js
+++ jquery.revealer.js
@@ -4,40 +4,48 @@
 
 (function( $ ) {
 	var methods = {
-		slide : function(x) {
+		slide : function(x, settings) {
 			if (x == null) {
 				return;
 			}
 			
-			if (x.css('display') != 'none') {
-				x.slideUp('fast');
+			if (x.is(':visible') && settings.perform != 'open') {
+				if (settings.willClose) { settings.willClose(); }
+				x.stop(true, true).slideUp(settings.duration, settings.easing, settings.onClose);
 			}
-			else {
-				x.slideDown('fast');
+			else if (settings.perform != 'close') {
+				if (settings.willOpen) { settings.willOpen(x); }
+				x.stop(true, true).slideDown(settings.duration, settings.easing, settings.onOpen);
 			}
 		},
-		none : function(x) { 
+		none : function(x, settings) {
 			if (x == null) {
 				return;
 			}
 			
-			if (x.css('display') != 'none') {
+			if (x.is(':visible') && settings.perform != 'open') {
+				if (settings.willClose) { settings.willClose(); }
 				x.hide();
+				if (settings.onClose) { settings.onClose(); }
 			}
-			else {
+			else if (settings.perform != 'close') {
+				if (settings.willOpen) { settings.willOpen(); }
 				x.show();
+				if (settings.onOpen) { settings.onOpen(); }
 			}
 		},
-		fade : function(x) { 
+		fade : function(x, settings) {
 			if (x == null) {
 				return;
 			}
 			
-			if (x.css('display') != 'none') {
-				x.fadeOut('fast');
+			if (x.is(':visible') && settings.perform != 'open') {
+				if (settings.willClose) { settings.willClose(); }
+				x.stop(true, true).fadeOut(settings.duration, settings.easing, settings.onClose);
 			}
-			else {
-				x.fadeIn('fast');
+			else if (settings.perform != 'close') {
+				if (settings.willOpen) { settings.willOpen(); }
+				x.stop(true, true).fadeIn(settings.duration, settings.easing, settings.onOpen);
 			}
 		}
 	};
@@ -47,28 +55,47 @@
 		var settings = $.extend( {
 			'clickable' : 'h3',
 			'content' 	: 'div',
-			'transition': 'slide'
+			'transition': 'slide',
+			'duration'  : 300,
+			'onOpen'	: function() {},
+			'onClose'	: function() {},
+			'willOpen'	: function() {},
+			'willClose'	: function() {},
+			'action'	: 'click',
+			'easing'	: 'swing',
+			'perform'	: 'toggle'
 		}, options);
-		 
+		
 		return this.each(function(){
 			var div = $(this);
-			div.find(settings.clickable).click(function() {
+			var fun = function(event) {
 				div.find(settings.content).each(function() {
 					switch(settings.transition) {
 						case 'slide':
-							methods.slide($(this));
+							event.preventDefault();
+							methods.slide($(this), settings);
 							break;
 						case 'fade':
-							methods.fade($(this));
+							event.preventDefault();
+							methods.fade($(this), settings);
 							break;
 						case 'none':
-							methods.none($(this));
+							event.preventDefault();
+							methods.none($(this), settings);
 							break;
 						default:
 							throw settings.transition + ' is not a supported transition. Use slide, fade or none';
 					}
 				});
-			});
+			}
+				
+					
+			if (settings.clickable == 'self' || settings.clickable == 'this' || div.is(settings.clickable)) {
+				div.bind(settings.action, fun);
+			}
+			else {
+				div.find(settings.clickable).bind(settings.action, fun);
+			}
 		});
 	};
 })( jQuery );
